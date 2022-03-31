9f72ad1d3283fc58e5f5b08183755af8ba547222
judy <592904519@qq.com>
2022-03-31 10:40:27 +0800

    conversion

--- conversion.py
+++ conversion.py
@@ -6,8 +6,8 @@
 class Solution(object):
     def __init__(self):
         self.dic = {
-            '0':[],
-            '1':[],
+            '0':[''],
+            '1':[''],
             '2': ['a', 'b', 'c'],
             '3': ['d', 'e', 'f'],
             '4': ['g', 'h', 'i'],
@@ -23,29 +23,17 @@ class Solution(object):
         :param digits: str
         :return: List[str]
         '''
-        result = []
-        tail = []
-        len_d = len(digits)
-        if len_d == 0:
-            return tail
-        elif len_d == 1:
+
+        if len(digits) == 1:
             return self.dic[digits]
-        else:
-            if '10' in digits or '01' in digits:
-                return tail
-            elif '1' in digits:
-                digits = digits.replace('1', '')
-                return self.dic[digits]
-            elif '0' in digits:
-                digits = digits.replace('0', '')
-                return self.dic[digits]
 
-        tail = self.conversion(digits[1:])
+        words = self.dic[digits[0]]
+        result = []
 
-        for i in self.dic[digits[0]]:
-            for j in tail:
+        for i in words:
+            for j in self.conversion(digits[1:]):
                 result.append(i + j)
         return result
 
 if __name__ == "__main__":
-    print(Solution().conversion('13'))
+    print(Solution().conversion('40'))
